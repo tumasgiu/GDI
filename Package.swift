47cd9fcc94ec8a9e89e0643690406253b27dfcaa
Thomas Franceschini <tumasgiu@mac.com>
2016-10-12 10:56:20 +0200

    Fix packages urls

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GDI",
     dependencies: [
-        .Package(url: "https://github.com/tumasgiu/CGDI", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/tumasgiu/Win32", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/tumasgiu/CGDI.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/tumasgiu/Win32.git", majorVersion: 0, minor: 2)
     ]
 )
